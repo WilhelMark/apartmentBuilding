2fd4f11db82c7edf98bdf036a6b4a21976f6621d
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-20 14:57:24 +0300

    Implement validation for apartment number input

--- apartmentBuilding.cpp
+++ apartmentBuilding.cpp
@@ -12,14 +12,23 @@ int main() {
     for (int i = 0; i < 10; i++) {
         cout << "Apartment " << (i + 1) << ": ";
         getline(cin, residents[i]); // Read the surname using Getline
-}
+    }
 
-// Request three numbers of the apartments by the user
-for (int i = 0; i < 3; i++) {
-    int apartmentNumber;
-    cout << "Enter apartment number (1-10): ";
-    cin >> apartmentNumber;
-}
+    // Request three numbers of the apartments by the user
+    for (int i = 0; i < 3; i++) {
+        int apartmentNumber;
+        cout << "Enter apartment number (1-10): ";
+        cin >> apartmentNumber;
+
+        // Check the correctness of the entered number of the apartment
+        if (apartmentNumber >= 1 && apartmentNumber <= 10) {
+            // We display the name of the tenant if the apartment number is correct
+            cout << "Resident of apartment " << apartmentNumber << ": " << residents[apartmentNumber - 1] << endl;
+        } else {
+            // We report an error if the apartment number is incorrect
+            cout << "Invalid apartment number. Please enter a number between 1 and 10." << endl;
+        }
+    }
 
     return 0;
 }
