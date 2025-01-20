a8468d63ea7850f36f05d93806358eaafaac7372
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-20 14:43:46 +0300

    Add initial code to create array and input resident names

---
+++ apartmentBuilding.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Create an array of lines for storing the names of the tenants
+    string residents[10];
+
+    // Entering the surnames of the residents of the apartments
+    cout << "Enter the last names of the residents for apartments 1 to 10:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "Apartment " << (i + 1) << ": ";
+        getline(cin, residents[i]); // Read the surname using Getline
+}
+
+    return 0;
+}
